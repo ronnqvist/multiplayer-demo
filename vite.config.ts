3ffccc5c0275b85323678900bedb2ea6fe3f3ef5
Simon Rönnqvist <simon@iki.fi>
2025-10-05 06:51:17 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/multiplayer-demo/', // Set the base path for GitHub Pages deployment
+  base: '/',
   build: {
-    outDir: 'docs', // Set output directory to 'docs' for GitHub Pages
+    outDir: 'docs',
   },
 })
